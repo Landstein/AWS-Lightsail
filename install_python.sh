f1999130a499c6901d510dbd0cead6bacffb935f
Landstein <eric@landsteinmail.com>
2020-02-09 09:19:06 -0500

    bash file to install python3.7

---
+++ install_python.sh
@@ -0,0 +1,7 @@
+echo "Installing Python3.7" 
+sudo apt update
+sudo apt install software-properities-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.7
+python3.7 –version 
+echo "python3.7 is installed"
