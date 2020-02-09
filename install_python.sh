37e8a632b6c33b5d4349396ce470b0f160eaa0af
Landstein <eric@landsteinmail.com>
2020-02-09 09:38:53 -0500

    python3.7 install and pip3 install

--- install_python.sh
+++ install_python.sh
@@ -4,5 +4,14 @@ sudo apt update
 sudo apt install software-properities-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.7
-python3.7 –version 
+python3.7 --version 
 echo "python3.7 is installed"
+echo "---------------------"
+echo "---------------------"
+echo "---------------------"
+echo "---------------------"
+echo "Installing pip3"
+sudo apt update
+sudo apt install python3-pip
+pip3 –version 
+echo "pip3 installed"
